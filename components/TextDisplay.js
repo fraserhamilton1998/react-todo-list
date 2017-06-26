9f715441b89b62a6b8968f95df51726ae90105cb
FraserH <fraserhamilton1998@gmail.com>
2017-06-26 15:16:10 +0100

    new

---
+++ components/TextDisplay.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class TextDisplay extends Component {
+    
+    render() {
+        return <div>I am Displaying Text: {this.props.text}</div>
+    }
+    
+}
+
+export default TextDisplay
