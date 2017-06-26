a5f6382fd2fb5103c2691eec05fddb1b197fedd5
FraserH <fraserhamilton1998@gmail.com>
2017-06-26 01:31:59 +0100

    cool

--- components/App.js
+++ components/App.js
@@ -1,1 +1,11 @@
+import React, { Component } from 'react'
 
+class App extends Component {
+    
+    render() {
+        return <div>This is definietly a react app!</div>    
+    }
+    
+}
+
+export default App
--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,20 @@
-
+module.exports = {
+    entry: ['./client/client.js'],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /nodule_modules/,
+                query: {
+                    presets: ['react', 'es2015']
+                }
+            }
+        ]
+    }
+}
